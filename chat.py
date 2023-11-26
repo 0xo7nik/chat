c8f24704b93b0b11cef245652e4b70d5a381bf34
Михаил <147162777+0xo7nik@users.noreply.github.com>
2023-11-26 19:09:53 +0300

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,55 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+import requests
+import json
+
+app = QApplication([])
+win = QWidget()
+win.setWindowTitle('Наш чат')
+win.resize(800, 500)
+
+vert_main = QVBoxLayout()
+hor_down = QHBoxLayout()
+hor_up = QHBoxLayout()
+main_chat = QPlainTextEdit()
+main_chat.setReadOnly(True)
+
+login = QLineEdit()
+login.setPlaceholderText('Логин')
+password = QLineEdit()
+password.setPlaceholderText('Пароль')
+name = QLineEdit()
+name.setPlaceholderText('Имя')
+msg = QLineEdit()
+msg.setPlaceholderText('Сообщение...')
+
+def send():
+    data1 = {'login': login.text(), 'password': password.text(), 'name': name.text(), 'message': msg.text()}
+    request_send = requests.post('http://algo.enotit.space/chatroom/send.php', data=data1).text
+    print(request_send)
+def update():
+    data1 = {'login': login.text(), 'password': password.text()}
+    request_take = requests.get('http://algo.enotit.space/chatroom/take.php', params=data1).text
+    main_chat.setPlainText(request_take)
+    print(data1)
+
+update_btn = QPushButton('Обновить')
+update_btn.clicked.connect(update)
+send_btn = QPushButton('Отправить')
+send_btn.clicked.connect(send)
+
+hor_up.addWidget(login, alignment=Qt.AlignTop)
+hor_up.addWidget(password, alignment=Qt.AlignTop)
+hor_up.addWidget(name, alignment=Qt.AlignTop)
+
+hor_down.addWidget(update_btn, alignment=Qt.AlignBottom)
+hor_down.addWidget(msg, alignment=Qt.AlignBottom)
+hor_down.addWidget(send_btn, alignment=Qt.AlignBottom)
+
+vert_main.addLayout(hor_up)
+vert_main.addLayout(hor_down)
+vert_main.addWidget(main_chat)
+
+win.setLayout(vert_main)
+win.show()
+app.exec_()
